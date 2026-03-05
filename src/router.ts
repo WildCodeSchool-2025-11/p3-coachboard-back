479c7fbffc658fe54a054453c01cc1ac993d7db9
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-03-05 23:01:58 +0100

    feat(eleves): ajout des routes et repository eleves

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
+import elevesRoutes from "./modules/eleves/elevesRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
+router.use("/api/eleves", elevesRoutes);
 
 export default router;
