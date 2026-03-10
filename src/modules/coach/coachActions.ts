0045e73f8145c25c32c4b34b13d5fa8ebc187365
Kevan Gautier <kevangautier@gmail.com>
2026-03-10 14:31:13 +0100

    feat(coach): add the function findAll

--- src/modules/coach/coachActions.ts
+++ src/modules/coach/coachActions.ts
@@ -1,2 +1,11 @@
 import type { RequestHandler } from "express";
 import * as coachRepository from "../coach/coachRepository.js";
+
+export const findAll: RequestHandler = async (_req_, res, next) => {
+	try {
+		const [rows] = await coachRepository.findAll();
+		res.json(rows);
+	} catch (err) {
+		next(err);
+	}
+};
