ee2f463d9e1cee0d11f63497cfdbe58f3531ff7b
Kevan Gautier <kevangautier@gmail.com>
2026-03-10 14:47:16 +0100

    feat(coach): add the function getById

--- src/modules/coach/coachActions.ts
+++ src/modules/coach/coachActions.ts
@@ -1,10 +1,24 @@
 import type { RequestHandler } from "express";
 import * as coachRepository from "../coach/coachRepository.js";
 
-export const findAll: RequestHandler = async (_req_, res, next) => {
+export const getAll: RequestHandler = async (_req, res, next) => {
 	try {
-		const [rows] = await coachRepository.findAll();
+		const rows = await coachRepository.findAll();
 		res.json(rows);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getById: RequestHandler = async (_req, res, next) => {
+	try {
+		const coach = await coachRepository.findById(String(_req.params.id));
+
+		if (!coach) {
+			res.sendStatus(404);
+			return;
+		}
+		res.json(coach);
 	} catch (err) {
 		next(err);
 	}
