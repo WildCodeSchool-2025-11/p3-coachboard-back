0a5155a054bddd66bbcc120a892ad410c5d174c2
Kevan Gautier <kevangautier@gmail.com>
2026-03-23 09:27:56 +0100

    feat: ajout du try&catch dans le middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,28 +3,32 @@ import type { RequestHandler } from "express";
 import * as authRepository from "../modules/auth/authRepository.js";
 
 export const checkLogin: RequestHandler = async (req, res, next) => {
-	const { email, mot_de_passe } = req.body;
+	try {
+		const { email, mot_de_passe } = req.body;
 
-	let user = await authRepository.findCoachByEmail(email);
-	let role = "coach";
+		let user = await authRepository.findCoachByEmail(email);
+		let role = "coach";
 
-	if (!user) {
-		user = await authRepository.findEleveByEmail(email);
-		role = "eleve";
+		if (!user) {
+			user = await authRepository.findEleveByEmail(email);
+			role = "eleve";
+		}
+
+		if (!user) {
+			res.status(401).json({ message: "Utilisateur introuvable" });
+			return;
+		}
+
+		const passwordValide = bcrypt.compareSync(mot_de_passe, user.MOT_DE_PASSE);
+
+		if (!passwordValide) {
+			res.status(401).json({ message: "Mauvais identifiants" });
+			return;
+		}
+
+		req.body.user = { ...user, role };
+		next();
+	} catch (err) {
+		next(err);
 	}
-
-	if (!user) {
-		res.status(401).json({ message: "Utilisateur introuvable" });
-		return;
-	}
-
-	const passwordValide = bcrypt.compareSync(mot_de_passe, user.MOT_DE_PASSE);
-
-	if (!passwordValide) {
-		res.status(401).json({ message: "Mauvais identifiants" });
-		return;
-	}
-
-	req.body.user = { ...user, role };
-	next();
 };
