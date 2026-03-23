b7d54f1e741681d6f35cc811cbdf0a2ccd1e4bbd
Kevan Gautier <kevangautier@gmail.com>
2026-03-23 09:43:48 +0100

    feat: modification de la variable secret

--- src/modules/auth/authActions.ts
+++ src/modules/auth/authActions.ts
@@ -5,13 +5,16 @@ export const signin: RequestHandler = async (req, res, next) => {
 	try {
 		const { user } = req.body;
 
+		const secret = process.env.JWT_SECRET;
+		if (!secret) throw new Error("JWT_SECRET manquant");
+
 		const token = jwt.sign(
 			{
 				id: user.role === "coach" ? user.ID_COACH : user.ID_ELEVE,
 				email: user.EMAIL,
 				role: user.role,
 			},
-			process.env.JWT_SECRET as string,
+			secret,
 			{ expiresIn: "24h" },
 		);
 		res.json({ token });
